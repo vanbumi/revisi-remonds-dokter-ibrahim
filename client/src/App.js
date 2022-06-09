33ff08bb90325d61b4e06459f3c8e00109c527d2
vanbumi <jsp.dio@gmail.com>
2022-06-09 15:58:49 +0700

    pdf 31 sudah sukses muncul landing nya

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Navbar from'./components/layout/Navbar';
 import Landing from'./components/layout/Landing';
 import React, { Fragment } from 'react';
 import'./App.css'; 
 
 const App= () => (
-  <Fragment>
-
-    <Navbar/>
-
-    <Landing/>
+  <Router>
+    <Fragment>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Landing/>}/>
+      </Routes>
+    </Fragment>)
+  </Router>
+);
   
-  </Fragment>)
-  
-  export default App;
+export default App;
